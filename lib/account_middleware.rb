f7c6d7502db8a9f0dc82a91eb03184fd256d742c
Patrick Palmer <ppalmer@pm.me>
2024-10-30 22:04:02 -0500

    Fix rubocop errors

--- lib/account_middleware.rb
+++ lib/account_middleware.rb
@@ -7,7 +7,7 @@ class AccountMiddleware
     request = ActionDispatch::Request.new(env)
 
     if m = /\A(\/(\d{1,}))/.match(request.path_info)
-      script_name, account_id, path_info = [m[1], m[2], m.post_match]
+      script_name, account_id, path_info = [ m[1], m[2], m.post_match ]
       request.script_name = script_name
       request.path_info   = path_info.presence || "/"
       set_current_account(account_id)
