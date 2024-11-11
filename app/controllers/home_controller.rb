3814e9d50bce4ef4788d53fc1a1a10ec81409ee4
Patrick Palmer <ppalmer@pm.me>
2024-11-11 17:33:57 -0600

    Add director dashboard stub

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,23 @@
 class HomeController < ApplicationController
   def index
-    # TODO: query data based on user role for dashboards
+    if Current.user.sysadmin?
+      @new_accounts = Account.order(created_at: :desc).limit(5)
+    end
+
+    if Current.user.tenant_admin?
+      @recent_users = User
+        .joins(:sessions)
+        .select("users.id, users.email, MAX(sessions.created_at) AS last_session_created_at")
+        .group("users.id")
+        .order("last_session_created_at DESC")
+        .limit(5)
+    end
+
+    if Current.user.director?
+      @upcoming_contests = Contest
+        .order("contest_start DESC")
+        .limit(5)
+    end
   end
 
   def settings
