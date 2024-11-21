6b13dc6830ef9efdda447ef3b6a0324690957653
Patrick Palmer <ppalmer@pm.me>
2024-11-21 08:23:30 -0600

    Add new users to tenant admin dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,19 @@ class HomeController < ApplicationController
   def index
     if Current.user.sysadmin?
       @new_accounts = Account.order(created_at: :desc).limit(5)
-    end
 
-    if Current.user.tenant_admin?
       @recent_users = User
         .joins(:sessions)
         .select("users.id, users.email, MAX(sessions.created_at) AS last_session_created_at")
         .group("users.id")
         .order("last_session_created_at DESC")
+        .limit(5)
+    end
+
+    if current_user.tenant_admin?
+      @new_users = User
+        .select("users.id, users.email, users.created_at")
+        .order("users.created_at DESC")
         .limit(5)
     end
 
