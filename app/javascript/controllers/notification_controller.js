2cfc702c430f95e5838452e8209ab57858df936e
Patrick Palmer <ppalmer@pm.me>
2024-11-24 14:08:43 -0600

    Remove notification console logs

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -5,29 +5,25 @@ export default class extends Controller {
   static values = { content: String };
 
   connect() {
-    console.log("Notification controller connected");
     this.show();
   }
 
   show() {
-    console.log("Notification show method called");
     this.messageTarget.textContent = this.contentValue || "Notification";
 
-    // Automatically hide after 3 seconds
     this.timeout = setTimeout(() => {
       this.hide();
     }, 5000);
   }
 
   hide() {
-    console.log("Notification hide method called");
-    
     // Clear the timeout if the user manually closes the notification
     if (this.timeout) clearTimeout(this.timeout);
 
     this.element.classList.add("opacity-0", "translate-y-2");
+
     setTimeout(() => {
       this.element.remove();
-    }, 300); // Allow time for the fade-out transition
+    }, 1000); // Allow time for the fade-out transition
   }
 }
