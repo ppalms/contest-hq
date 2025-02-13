fe439b2b8f96086dc4d7fdb21c8b220ed10d7166
Patrick Palmer <ppalmer@pm.me>
2025-02-13 01:30:18 -0600

    Remove performance_steps from room

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,6 @@ class Room < ApplicationRecord
   include AccountScoped
 
   belongs_to :contest
-  has_many :performance_steps, dependent: :delete_all
 
   validates :name, :room_number, presence: true
   validates :room_number, uniqueness: { scope: :contest_id }
