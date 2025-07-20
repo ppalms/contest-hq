678b5cf226160d2e3fa9d44093d581c529025e23
Patrick Palmer <ppalmer@pm.me>
2025-07-20 17:40:06 -0500

    Fix linting errors

--- app/controllers/contests/managers_controller.rb
+++ app/controllers/contests/managers_controller.rb
@@ -58,4 +58,3 @@ module Contests
     end
   end
 end
-
--- test/controllers/contests/managers_controller_test.rb
+++ test/controllers/contests/managers_controller_test.rb
@@ -3,10 +3,10 @@ require "test_helper"
 class Contests::ManagersControllerTest < ActionDispatch::IntegrationTest
   # Note: Due to testing environment limitations, these are basic syntax tests
   # In a real environment, these would be proper integration tests
-  
+
   test "controller class exists and has required methods" do
     controller = Contests::ManagersController.new
-    
+
     # Check that controller responds to expected methods
     assert_respond_to controller, :index
     assert_respond_to controller, :new
