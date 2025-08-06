67c128b1bb475b4e9c694a9667891f0a5b31aba5
Patrick Palmer <ppalmer@pm.me>
2025-08-06 00:33:11 -0500

    Fix raw SQL in migration (#166)
    * Fix raw SQL in migration

--- db/migrate/20250131120002_add_season_to_contests.rb
+++ db/migrate/20250131120002_add_season_to_contests.rb
@@ -9,7 +9,7 @@ class AddSeasonToContests < ActiveRecord::Migration[8.0]
       contest_years = Contest.where(account: account)
                            .where.not(contest_start: nil)
                            .distinct
-                           .pluck("EXTRACT(YEAR FROM contest_start)::integer")
+                           .pluck(Arel.sql("EXTRACT(YEAR FROM contest_start)::integer"))
                            .sort
 
       # If no contests have dates, create a current year season
@@ -23,9 +23,8 @@ class AddSeasonToContests < ActiveRecord::Migration[8.0]
           archived: year < Date.current.year
         )
 
-        # Assign contests from this year to this season
         Contest.where(account: account)
-               .where("EXTRACT(YEAR FROM contest_start) = ?", year)
+               .where(Arel.sql("EXTRACT(YEAR FROM contest_start) = ?"), year)
                .update_all(season_id: season.id)
       end
 
@@ -42,7 +41,6 @@ class AddSeasonToContests < ActiveRecord::Migration[8.0]
   end
 
   def down
-    remove_index :contests, :season_id
     remove_reference :contests, :season, foreign_key: true
   end
 end
