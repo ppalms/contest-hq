22d2720c7f852aa5c6f4ab69f767464dc8b35a32
Patrick Palmer <ppalmer@pm.me>
2025-08-30 08:52:34 -0500

    Fix linting errors

--- test/controllers/schedules_controller_test.rb
+++ test/controllers/schedules_controller_test.rb
@@ -3,42 +3,42 @@ require "test_helper"
 class SchedulesControllerTest < ActionDispatch::IntegrationTest
   test "manager can view schedule for contest they manage" do
     sign_in_as users(:demo_manager_a)
-    
+
     get schedule_path(schedules(:demo_district_schedule))
-    
+
     assert_response :success
   end
 
   test "manager cannot view schedule for contest they don't manage" do
     sign_in_as users(:demo_manager_b)
-    
+
     get schedule_path(schedules(:demo_district_schedule))
-    
+
     assert_redirected_to root_path
     assert_equal "You do not have permission to view this schedule", flash[:alert]
   end
 
   test "director can view schedule for contest they have entries in" do
     sign_in_as users(:demo_director_a)
-    
+
     get schedule_path(schedules(:demo_district_schedule))
-    
+
     assert_response :success
   end
 
   test "admin can view any schedule" do
     sign_in_as users(:demo_admin_a)
-    
+
     get schedule_path(schedules(:demo_district_schedule))
-    
+
     assert_response :success
   end
 
   test "user from different account cannot view schedule" do
     sign_in_as users(:customer_director_a)
-    
+
     get schedule_path(schedules(:demo_district_schedule))
-    
+
     assert_response :not_found
   end
 end
--- test/models/contest_test.rb
+++ test/models/contest_test.rb
@@ -15,18 +15,18 @@ class ContestTest < ActiveSupport::TestCase
       account: accounts(:demo),
       season: seasons(:demo_2024)
     )
-    
+
     assert_not contest.setup_complete?
   end
 
   test "setup_complete? returns false when setup is not complete" do
     # For the existing contest, ensure it's not complete by default
     # (we'll assume the fixtures don't have full setup)
-    
+
     # If it has rooms and phases, remove them for this test
     @contest.performance_phases.destroy_all
     @contest.rooms.destroy_all
-    
+
     assert_not @contest.setup_complete?
   end
 
@@ -34,12 +34,12 @@ class ContestTest < ActiveSupport::TestCase
     # Create room and performance phase for this test
     room = @contest.rooms.create!(name: "Test Room", room_number: "101")
     @contest.performance_phases.create!(
-      name: "Performance", 
-      duration: 15, 
+      name: "Performance",
+      duration: 15,
       ordinal: 1,
       room: room
     )
-    
+
     assert @contest.setup_complete?
   end
 
@@ -47,7 +47,7 @@ class ContestTest < ActiveSupport::TestCase
     # Clear rooms and phases
     @contest.rooms.destroy_all
     @contest.performance_phases.destroy_all
-    
+
     assert_equal "Needs Setup", @contest.setup_status
   end
 
@@ -55,15 +55,15 @@ class ContestTest < ActiveSupport::TestCase
     # Ensure setup is complete
     room = @contest.rooms.create!(name: "Test Room", room_number: "101")
     @contest.performance_phases.create!(
-      name: "Performance", 
-      duration: 15, 
+      name: "Performance",
+      duration: 15,
       ordinal: 1,
       room: room
     )
-    
+
     # Ensure no schedule days
     @contest.schedules.each { |schedule| schedule.schedule_days.destroy_all }
-    
+
     assert_equal "Ready to Schedule", @contest.setup_status
   end
 
@@ -71,12 +71,12 @@ class ContestTest < ActiveSupport::TestCase
     # Ensure setup is complete
     room = @contest.rooms.create!(name: "Test Room", room_number: "101")
     @contest.performance_phases.create!(
-      name: "Performance", 
-      duration: 15, 
+      name: "Performance",
+      duration: 15,
       ordinal: 1,
       room: room
     )
-    
+
     # Create a schedule with days
     schedule = @contest.schedules.first || @contest.create_schedule!
     schedule.schedule_days.create!(
@@ -84,7 +84,7 @@ class ContestTest < ActiveSupport::TestCase
       start_time: @contest.contest_start.beginning_of_day + 8.hours,
       end_time: @contest.contest_start.beginning_of_day + 17.hours
     )
-    
+
     assert_equal "Scheduled", @contest.setup_status
   end
 end
