62a0da30f817c1f3967f8c827486a416f329faa7
Patrick Palmer <ppalmer@pm.me>
2025-10-01 15:29:22 -0500

    Add SES logs to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
     region: "us-east-2"
   }
 
+  Rails.logger.info "SES credentials check: #{Rails.application.credentials.dig(:aws_ses, :user_name).present? ? 'user_name present' : 'user_name MISSING'}"
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
